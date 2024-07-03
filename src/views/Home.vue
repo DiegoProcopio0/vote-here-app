1bad77ab887d6e07eb06f82ea8fd3180da04e056
Diego Silva <diegprocopio98@gmail.com>
2024-07-02 22:19:39 -0300

    feat: wip

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,12 +8,14 @@
   <main
     class="surface-card px-4 pt-6 shadow-2 border-round w-full h-full flex flex-col justify-center items-center mx-auto"
   >
-    <header class="flex justify-between items-center gap-16 w-full px-4">
-      <h1 class="text-2xl text-bold">
+    <header
+      class="flex justify-between items-center gap-6 w-full px-4 flex-col md:flex-row"
+    >
+      <h1 class="text-xl md:text-2xl text-center text-bold">
         Visualize as melhores perguntas do momento!
       </h1>
       <div class="flex items-center gap-2">
-        <IconField>
+        <IconField class="max-w-">
           <InputIcon class="pi pi-search" />
           <InputText v-model="value1" placeholder="Search" />
         </IconField>
